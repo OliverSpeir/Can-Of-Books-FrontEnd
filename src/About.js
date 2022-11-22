3a3081c89208aad5face406ac652493b165dd578
Oliver Speir <oliverspeir9@gmail.com>
2022-11-22 00:41:20 +0000

    completed lab 11 without any css

--- src/About.js
+++ src/About.js
@@ -3,8 +3,12 @@ import { Component } from "react";
 class Profile extends Component {
 
   render() {
-    /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+      <h1> Authors: </h1>
+      <h2> Cody Delatorre and Oliver Speir</h2>
+      </>
+    )
   }
 };
 
